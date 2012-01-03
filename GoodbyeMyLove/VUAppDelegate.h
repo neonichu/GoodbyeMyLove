fae383e57acb552aae01d07c89e19633a169cde4
Boris Bügling <boris@icculus.org>
2012-01-03 09:39:58 +0100

    Initial Commit

---
+++ GoodbyeMyLove/VUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VUAppDelegate.h
+//  GoodbyeMyLove
+//
+//  Created by Boris Bügling on 03.01.12.
+//  Copyright (c) 2012 Extessy AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GoodbyeMyLove/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GoodbyeMyLove
+//
+//  Created by Boris Bügling on 03.01.12.
+//  Copyright (c) 2012 Extessy AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VUAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VUAppDelegate class]));
+    }
+}
